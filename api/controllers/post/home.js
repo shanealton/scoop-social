574a7e4af7f67f9c51805f03301f444a4cbb7c79
Shane Alton <shanealton1@gmail.com>
2021-07-01 15:13:12 -0400

    Add User > Post relationship
    -Associate the user with their posts and reflect that inside the new post object.

--- api/controllers/post/home.js
+++ api/controllers/post/home.js
@@ -1,7 +1,8 @@
 module.exports = async function(req, res) {
   sails.log.info("Show the post creation form")
 
-  const allPosts = await Post.find().sort('createdAt DESC')
+  const userId = req.session.userId
+  const allPosts = await Post.find({user: userId}).populate('user').sort('createdAt DESC')
   res.view('pages/post/home', {
     allPosts
   })
