52433e3531c58ac1153fca2a942e7ee50c9e3349
Shane Alton <shanealton1@gmail.com>
2021-10-13 12:00:45 -0400

    Add wantsJSON
    -Added wantsJSON in sails app for serving JSON in the iOS application

--- api/controllers/post/home.js
+++ api/controllers/post/home.js
@@ -3,6 +3,11 @@ module.exports = async function(req, res) {
 
   const userId = req.session.userId
   const allPosts = await Post.find({user: userId}).populate('user').sort('createdAt DESC')
+
+  if (req.wantsJSON) {
+    return res.send(allPosts)
+  }
+
   res.view('pages/post/home', {
     allPosts
   })
