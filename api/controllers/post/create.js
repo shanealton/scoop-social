eb92bfb4d8066a263afeb80e4d0e75fed659b72d
Shane Alton <shanealton1@gmail.com>
2021-10-14 22:54:16 -0400

    Add AWS S3 image posts
    -Added AWS S3 support for posting images.
    
    -Added local env variables
    
    -Added Skipper-Better-S3 for public access to posted images
    
    -Added imageUrl to Post model

--- api/controllers/post/create.js
+++ api/controllers/post/create.js
@@ -2,8 +2,45 @@ module.exports = async function(req, res) {
   const postBody = req.body.postBody
   sails.log.info("Create post object: " + postBody)
 
-  //Waterline: create promise object for post body
-  const userId = req.session.userId
-  await Post.create({text: postBody, user: userId}).fetch()
-  res.redirect('/post')
+  const file = req.file('imagefile')
+
+  //Local ENV-var
+  const key = process.env.AWS_KEY
+  const secret = process.env.AWS_SECRET
+
+  //Skipper-Better-S3
+  const options =
+  { // This is the usual stuff
+    adapter: require('skipper-better-s3')
+    , key: key
+    , secret: secret
+    , bucket: 'scoop-social'
+    // Let's use the custom s3params to upload this file as publicly
+    // readable by anyone
+    , s3params:{ ACL: 'public-read' }
+    // And while we are at it, let's monitor the progress of this upload
+    , onProgress: progress => sails.log.verbose('Upload progress:', progress)
+  }
+
+  file.upload(options, async (err, files) => {
+    if (err) { return res.serverError(err.toString())}
+    //Success
+
+    var fileUrl = ''
+    try {
+       fileUrl = files[0].extra.Location
+     } catch (error) {
+       fileUrl = ''
+     }
+
+    const userId = req.session.userId
+
+    await Post.create({
+      text: postBody,
+      user: userId,
+      imageUrl: fileUrl
+    }).fetch()
+
+    res.redirect('/post')
+  })
 }
